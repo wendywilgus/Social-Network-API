d5fda715aee69d51181b7c7ea9217bb8f8bd5158
Wendy Wilgus <wilguswendy@gmail.com>
2022-11-08 09:38:02 -0500

    added reaction.js

--- models/reaction.js
+++ models/reaction.js
@@ -1,1 +1,33 @@
-const 
+const { Schema, Types } = require('mongoose');
+const { $where } = require('./thoughts');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: date => {
+            const newDate = new Date(date);
+            return `${newDate.getMonth()+1}-${newDate.getDate()}-${newDate.getFullYear()}`
+        }
+    }
+}, {
+    toJson: {
+        getters: true
+    },
+    id: false
+});
+
+module.exports = reactionSchema;
